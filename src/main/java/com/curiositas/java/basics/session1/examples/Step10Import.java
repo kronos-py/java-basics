1e28c62e6f734534c5ed780e2250f9355d0ad6e0
dgorbatykh <dgorbatykh@plesk.com>
2022-07-02 23:44:07 +0200

    Adding step 10

---
+++ src/main/java/com/curiositas/java/basics/session1/examples/Step10Import.java
@@ -0,0 +1,17 @@
+package com.curiositas.java.basics.session1.examples;
+
+
+import com.curiositas.java.basics.session1.examples.imports.Date;
+
+public class Step10Import {
+    public static void main(String[] args) {
+
+        var date = new Date();
+        System.out.println(date);
+
+        var anotherDate = new java.util.Date();
+        System.out.println(anotherDate);
+
+        System.out.println(Math.abs(-1));
+    }
+}
---
+++ src/main/java/com/curiositas/java/basics/session1/examples/imports/Date.java
@@ -0,0 +1,8 @@
+package com.curiositas.java.basics.session1.examples.imports;
+
+public class Date {
+    @Override
+    public String toString() {
+        return "Sat Jan 01 23:42:09 CEST 2000";
+    }
+}
